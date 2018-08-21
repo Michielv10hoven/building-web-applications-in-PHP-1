acc5abe41c90aff2e013cdcae2ce4e8e930b3d55
Michielv10hoven <37332723+Michielv10hoven@users.noreply.github.com>
2018-08-21 14:43:40 +0200

    Adding followed tutorial files to repo, from desktop
    Early stages of tutorial I followed, now also added to repo

---
+++ Second/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Michiel van Tienhoven PHP</title>
+</head>
+
+<body>
+<h1>Michiel van Tienhoven PHP</h1>
+<pre>
+O~~       O~~
+O~ O~~   O~~~
+O~~ O~~ O O~~
+O~~  O~~  O~~
+O~~   O~  O~~
+O~~       O~~
+O~~       O~~
+
+<?php 
+print hash('sha256', 'Michiel van Tienhoven')
+?>
+</pre>
+
+</body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,2 @@
+<h1> <?php $stuff = array('course' => 'PHP-Intro', 'topic' => 'Arrays');
+echo isset($stuff['section']); ?> </h1>
